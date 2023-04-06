89499a0747b84ff8c6514a24c87cfda8f8211761
Katrin <126097484+Katrin2108@users.noreply.github.com>
2023-04-06 17:23:09 +0400

    end

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log("text from js.file");
+
+var studentName = "Ekaterine";
+
+let studentName2 ="katrin";
+const studentName3="Ekaterine";
+
+const userAge= "20";
+const averageScore="50";
+const userName="Katrin";
+const userEmail="e.mushkudiani88@gmail.com";
+const isStudent="true";
+const isEmployed="false";
+const myNull=null;
+
+const myComputerManufacturer="Hp Zbool";
+console.log ("my computer is");
+const myComputerMemory="64GB";
+const myComputerOS=" Hp core i7";
+console.log ("it runs on")
+const computerBuyingYear="2021";
+const currentYear="2023";
+console.log("and I've been using it for 2 year");
+
